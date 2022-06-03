108f207ae348cd0fb032ae27c5ca07e0441302f6
AleBitetto <bitettoalessandro@gmail.com>
2022-06-03 10:08:56 +0200

    added stategy upload

--- utils.py
+++ utils.py
@@ -9,6 +9,8 @@ from selenium.common.exceptions import NoAlertPresentException
 import time, re
 from timeit import default_timer as timer
 import datetime
+from bs4 import BeautifulSoup
+import autoit
 
 
 def get_chromedriver(chromedriver_path=None, use_proxy=False, user_agent=None,
@@ -227,3 +229,145 @@ def scrape_from_Orbis(chromedriver_path=None, PROXY_HOST=None, PROXY_PORT=None, 
     #     time.sleep(60*3)
     #     driver.find_element_by_xpath("//img[2]").click()
     #     driver.find_element_by_link_text("Disconnetti").click()
+    
+    
+
+def generate_strategy(nation = "9IT", nace = "01"):
+    # multiple values for "nace" must be saparated by ";"
+
+    SAMPLE_STRATEGY = "Sample_Strategy.strategy"       # sample strategy file
+    DATE_TO_REPLACE = "Tuesday, 12 April 2022 16:59:21"     # date reported in SAMPLE_STRATEGY
+    NATION_TO_REPLACE = "9IT"                               # nation reported in SAMPLE_STRATEGY
+    NACE_TO_REPLACE = "4941;495;52;551;553;552"             # NACE reported in SAMPLE_STRATEGY
+
+    with open(SAMPLE_STRATEGY) as f:
+        strategy_lines = f.readlines()
+    
+    strategy_lines = [s.replace(DATE_TO_REPLACE, datetime.datetime.now().strftime("%A, %m %B %Y %H:%M:%S")) for s in strategy_lines]
+    strategy_lines = [s.replace('<Action Type="SingleAdd" ItemId="'+NATION_TO_REPLACE+'" />', '<Action Type="SingleAdd" ItemId="'+nation+'" />') for s in strategy_lines]
+    strategy_lines = [s.replace('<Action Type="SingleAdd" Array="raw_semicolon_separated" ItemId="'+NACE_TO_REPLACE+'" />', '<Action Type="SingleAdd" Array="raw_semicolon_separated" ItemId="'+nace+'" />') for s in strategy_lines]
+
+    return strategy_lines
+
+
+
+def save_strategy(nation = "9IT", strategy_folder = "Strategy",
+                  NACE_html_list = "Nace_rev2.html", output_text_separator = ";"):
+
+    DESCRIPTION_FILE = "NACE_description.csv"
+    
+    # create strategy folder
+    if not os.path.exists(strategy_folder):
+        os.makedirs(strategy_folder)
+
+    # read list of NACE codes and description
+    HTMLFile = open(NACE_html_list, "r", encoding='utf-8')
+    index = HTMLFile.read()
+    soup = BeautifulSoup(index, 'html.parser')
+    for script in soup(["script", "style"]):
+        script.extract()  
+    text = soup.get_text()
+    sectors = (line.strip() for line in text.splitlines())
+    sectors = [l for l in sectors if l != '']
+    sectors = [l.split(" - ") for l in sectors]
+
+    # create strategy files
+    nace_list = ['"NACE"'+output_text_separator+'"Description"']
+    for sec in sectors:
+        nace = sec[0]
+        descr = sec[1]
+
+        # generate strategy
+        strat_lines = generate_strategy(nation = nation, nace = nace)
+        strat_name = 'Nation_'+nation+'_NACE_'+nace+'.strategy'
+        with open(os.path.join(strategy_folder, strat_name), 'w') as f:
+            f.write(''.join([s for s in strat_lines]))
+
+        # update list of nace and description
+        nace_list.append('"'+nace+'"'+output_text_separator+'"'+descr+'"')
+
+    # save nace description
+    with open(DESCRIPTION_FILE, 'w') as f:
+        f.write('\n'.join(nace_list))
+        
+    print('- '+str(len(sectors))+' strategies generated. Saved in "'+strategy_folder+'"')
+    print('- NACE sectors description saved in "'+DESCRIPTION_FILE+'"\n')
+    
+    
+    
+def upload_strategy(chromedriver_path=None, PROXY_HOST=None, PROXY_PORT=None, PROXY_USER=None, PROXY_PASS=None,
+                    Orbis_landing_page=None, Orbis_username=None, Orbis_pass=None, strategy_folder=None):
+    
+    # define strategy to upload
+    file_list = os.listdir(strategy_folder)
+    file_list = ['"' + os.path.join(os.getcwd(), strategy_folder, f) + '"' for f in file_list]
+    max_string_len = max([len(x) for x in file_list]) + 1
+    path_chunk_size = 259 // max_string_len     # upload window can handle max 259 chars
+    upload_path = [" ".join(file_list[i:(i+path_chunk_size)]) for i in range(0, len(file_list), path_chunk_size)]
+    
+    # open browser and go to website
+    print("Launching Chrome...", end ="")
+    driver = get_chromedriver(chromedriver_path = chromedriver_path, use_proxy=True,
+                              PROXY_HOST=PROXY_HOST, PROXY_PORT=PROXY_PORT, PROXY_USER=PROXY_USER, PROXY_PASS=PROXY_PASS)
+    driver.implicitly_wait(30)
+    driver.get(Orbis_landing_page)
+    print("OK", end ="\n")
+
+    # login
+    print("Logging in...", end ="")
+    driver.find_element_by_id("user").clear()
+    driver.find_element_by_id("user").send_keys(Orbis_username)
+    driver.find_element_by_id("pw").clear()
+    driver.find_element_by_id("pw").send_keys(Orbis_pass)
+    driver.find_element_by_id("user").click()
+    driver.find_element_by_id("loginPage").click()
+    driver.find_element_by_xpath("//div[@id='loginPage']/div[2]").click()
+    driver.find_element_by_xpath("//div[@id='loginPage']/div[2]/div[3]/button").click()
+    time.sleep(2)
+    driver.find_element_by_xpath("//input[@value='Accept']").click()
+    # check for session restart
+    try:
+        driver.find_element_by_xpath("//input[@value='Restart']").click()
+        print("SESSION RESTART DONE", end ="\n")
+    except:
+        print("OK", end ="\n")
+
+    # open search tab
+    print("Opening search tab...", end ="")
+    driver.find_element_by_xpath("//div[@id='search-toolbar']/div/div/ul/li[2]/h4").click()
+    print("OK", end ="\n")
+
+    # upload strategies
+    for up_i, up in enumerate(upload_path):
+        print("Uploading strategy chunk", str(up_i+1), "/", str(len(upload_path)), end="... ")
+        
+        driver.find_element_by_link_text("Importa strategia di ricerca").click()
+        driver.find_element_by_id("upload-file-trigger").click()
+        time.sleep(1.5)
+        autoit.win_active("Open")
+        time.sleep(1.5)
+        autoit.control_send("Open","Edit1",up)
+        autoit.control_send("Open","Edit1","{ENTER}")
+
+        # check for existing strategy
+        time.sleep(2)
+        try:
+            elem = driver.find_elements_by_css_selector("div.buttons.popup__buttons")
+            # search for "Sovrascrivi tutto"
+            stop = False
+            for el in elem:
+                if re.search('<a class="button overwrite-all">Sovrascrivi tutto</a>', el.get_attribute("outerHTML")):
+                    driver.find_element_by_link_text("Sovrascrivi tutto").click()
+                    print("MULTIPLE STRATEGIES OVERWRITTEN", end="\n")
+                    stop = True
+            # search for "Sovrascrivi"
+            if not stop:
+                driver.find_element_by_link_text("Sovrascrivi").click()
+                print("SINGLE STRATEGY OVERWRITTEN", end="\n")
+        except:
+            print("OK", end ="\n")   
+        time.sleep(1.5)           
+    
+    # close browser
+    driver.close()
+
